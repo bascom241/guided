2fab8b2b0418ae20d83bf828f49fb3ef32e942cf
Abdulbasit <135514268+bascom241@users.noreply.github.com>
2025-04-13 06:51:28 +0100

    Update axios.ts

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://guided-backend-9hw5.onrender.com/api",
     withCredentials:true
 })
